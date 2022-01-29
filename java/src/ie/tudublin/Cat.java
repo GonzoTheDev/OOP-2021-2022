5f07e16a2dd2f769f94e8ada3dda19c32dea547a
GonzoTheDev <c20703429@mytudublin.ie>
2022-01-29 12:43:13 +0000

    killing the cat

--- java/src/ie/tudublin/Cat.java
+++ java/src/ie/tudublin/Cat.java
@@ -2,8 +2,36 @@ package ie.tudublin;
 
 public class Cat extends Animal
 {
+    private int numLives;
+    
+    public int getNumLives() {
+        return numLives;
+    }
+
+    public void setNumLives(int numLives) {
+        this.numLives = numLives;
+    }
+
+    public Cat(String name, int numLives) {
+        super(name);
+        this.numLives = numLives;
+    }
+
     public Cat(String name)
     {
         super(name);
+        numLives = 9;
+    }
+
+    public void killMe()
+    {
+        if(this.numLives <= 0)
+        {
+            System.out.println("Dead.");
+        } else {
+            System.out.println("Ouch.");
+        }
+
+        this.numLives -= 1;
     }
 }
--- java/src/ie/tudublin/Main.java
+++ java/src/ie/tudublin/Main.java
@@ -9,6 +9,21 @@ public class Main
     }
     public static void main(String[] args)
     {
-        helloProcessing();
+        //helloProcessing();
+        System.out.println("Hello World.");
+        Animal misty = new Animal("Misty");
+        Animal lucy = new Animal("Lucy");
+
+        lucy = misty;
+        misty.setName("Tara");
+
+        System.out.println(misty);
+        System.out.println(lucy);
+        Cat ginger = new Cat("Ginger");
+
+        for( int i = 0; i < 10; i++) 
+        {
+            ginger.killMe();
+        }
     }
 }
