80b14d761ce84339f736bda09ef4a1ad09593ab1
GonzoTheDev <c20703429@mytudublin.ie>
2022-02-02 20:53:14 +0000

    Initial setup

---
+++ java/src/ie/tudublin/BugZap.java
@@ -0,0 +1,23 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class BugZap extends PApplet {
+
+    float playerX, playerY, playerWidth;
+
+    public void settings()
+	{
+        size(500,500);   
+	}
+
+	public void setup() 
+    {
+		
+	}
+	
+	public void draw()
+	{	
+		
+	}
+}
