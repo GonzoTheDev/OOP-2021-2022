d01ff96445cedcddc3ac232325e6b047c0b0e6f5
GonzoTheDev <c20703429@mytudublin.ie>
2022-02-03 09:24:13 +0000

    Drawing player

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -2,7 +2,8 @@ package ie.tudublin;
 
 import processing.core.PApplet;
 
-public class BugZap extends PApplet {
+public class BugZap extends PApplet 
+{
 
     float playerX, playerY, playerWidth;
 
@@ -13,11 +14,30 @@ public class BugZap extends PApplet {
 
 	public void setup() 
     {
-		
+		colorMode(RGB);
+
+		smooth();
+
+		playerX = width / 2; // Width is a built in variable of PApplet which is the width of the screen
+		playerY = height - 50; // Height is another built in variable
+		playerWidth = 50;
+
 	}
 	
+	void drawPlayer(float x, float y, float w) 
+	{
+		stroke(255);
+		rectMode(CENTER);
+		noFill();
+		rect(x, y, w, 20);
+		line(x, y - 10, x, y - 20);
+
+	}
+
 	public void draw()
 	{	
-		
+		background(0);
+		strokeWeight(2);
+		drawPlayer(playerX, playerY, playerWidth);
 	}
 }
