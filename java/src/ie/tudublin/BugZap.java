28e060447a32a36125f6fb83625960e2044016ed
GonzoTheDev <c20703429@mytudublin.ie>
2022-02-03 09:41:31 +0000

    Bug moving

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -5,13 +5,19 @@ import processing.core.PApplet;
 public class BugZap extends PApplet 
 {
 
+	// Player variables
     float playerX, playerY, playerWidth;
 
+	// Bug variables
+	float bugX, bugY, bugWidth;
+
+	// Settings method
     public void settings()
 	{
         size(500,500);   
 	}
 
+	// Setup method
 	public void setup() 
     {
 		colorMode(RGB);
@@ -22,8 +28,24 @@ public class BugZap extends PApplet
 		playerY = height - 50; // Height is another built in variable
 		playerWidth = 50;
 
+		resetBug();
 	}
 	
+	private void resetBug() 
+	{
+		bugX = random(bugWidth / 2, width - (bugWidth /2));
+		bugY = 50;
+		bugWidth = 50;
+	}
+
+	void drawBug(float x, float y, float w) 
+	{
+		float halfW = w / 2;
+		stroke(255);
+		noFill();
+		triangle(x - halfW, y + halfW, x, y - halfW, x + halfW, y + halfW);
+	}
+
 	void drawPlayer(float x, float y, float w) 
 	{
 		stroke(255);
@@ -34,10 +56,35 @@ public class BugZap extends PApplet
 
 	}
 
+	float playerSpeed = 5;
+
+	public void keyPressed() 
+	{
+		if(keyCode == LEFT) // keyCode is built in variable to get keys pressed from keyboard
+		{
+			playerX -=playerSpeed;
+		}
+		if(keyCode == RIGHT)
+		{
+			playerX+=playerSpeed;
+		}
+	}
+
+	void moveBug()
+	{
+		bugY++;
+		bugX += random(-20, 20);
+	}
+
 	public void draw()
 	{	
 		background(0);
 		strokeWeight(2);
 		drawPlayer(playerX, playerY, playerWidth);
+		drawBug(bugX, bugY, bugWidth);
+		if (frameCount % 20 == 0) 
+		{
+			moveBug();
+		}
 	}
 }
