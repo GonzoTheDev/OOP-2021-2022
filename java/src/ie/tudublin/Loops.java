9d90a5e87f54fad3952ccf2d8893f01d183e2a11
GonzoTheDev <c20703429@mytudublin.ie>
2022-02-09 20:31:30 +0000

    Lab3 Part 2
    Add the first part into the switch statement, create the second part of the task and add that to the switch statement.

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -32,32 +32,56 @@ public class Loops extends PApplet
 	{
 		background(0);
 
-		int max = 0;
-		int rec1 = 0;
-		int rec2 = 50;
-
-        for(int i = 0; i < 10;  i++)
-		{
-			
-			if(i > 0)
-			{
-				max = max + 20;
-				rec1 = rec1 + 50;
-				rec2 = rec2 + 50;
-			}
-			noStroke();
-            fill(max, 300, 300);
-			rect(rec1, 0, rec2, 500);
-
-            
-        }
+		
 
 
 		switch(mode)	
 		{
 			case 0:
+				int max = 0;
+				int rec1 = 0;
+				int rec2 = 50;
+		
+				for(int i = 0; i < 10;  i++)
+				{
+					
+					if(i > 0)
+					{
+						max = max + 20;
+						rec1 = rec1 + 50;
+						rec2 = rec2 + 50;
+					}
+					noStroke();
+					fill(max, 300, 300);
+					rect(rec1, 0, rec2, 500);
+		
+					
+				}
 				break;
 			case 1:
+				int color = 0;
+				int square1 = 0;
+				int square2 = 50;
+				int square3 = 0;
+				int square4 = 50;
+		
+				for(int i = 0; i < 10;  i++)
+				{
+					
+					if(i > 0)
+					{
+						color = color + 20;
+						square1 = square1 + 50;
+						//square2 = square2 + 50;
+						square3 = square3 + 50;
+						//square4 = square4 + 50;
+					}
+					noStroke();
+					fill(color, 300, 300);
+					rect(square1, square3, square2, square4);
+		
+					
+				}
 				break;
 		}
 	}
