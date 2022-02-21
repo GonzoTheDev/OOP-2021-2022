9ed1ce2df4ea52138d1b565c7337883c585444ae
GonzoTheDev <c20703429@mytudublin.ie>
2022-02-21 09:29:07 +0000

    Update Loops.java

--- java/src/ie/tudublin/Loops.java
+++ java/src/ie/tudublin/Loops.java
@@ -69,7 +69,7 @@ public class Loops extends PApplet
 				
 				break;
 			case 2:
-				background(0);
+				background(255);
 				int circles = (int) (mouseX / 20.0f);
 				float radius = width / (float)circles;	
 				
@@ -77,12 +77,42 @@ public class Loops extends PApplet
 				{
 					noStroke();
 					fill(map(i, 0, circles, 0, 255), 255, 255);
-					//float x = map(i, 0, circles, 0, width);
 					circle(map(i, 0, circles - 1, radius / 2.0f, width - (radius / 2.0f)), height / 2, radius);
 
 				}
 				break;
+			case 3:
+				background(255);
+				int circles2 = (int) (mouseX / 20.0f);
+				float radius2 = width / (float)circles2;	
 				
+				for(int i = 0; i < circles2; i++)
+				{
+					for(int j = 0; j < circles2; j++)
+					{
+						noStroke();
+						fill(map(i, 0, circles2, 0, 255), 255, 255);
+						circle(map(i, 0, circles2 - 1, radius2 / 2.0f, width - (radius2 / 2.0f)), height / (float) j, radius2);
+					}
+
+				}
+				break;
+			case 4:
+				background(0);
+				int squares = 10;
+				float w2 = width / (float)squares;	
+				for(int i = 0 ; i < squares ; i ++)
+				{
+					size(400, 400);
+					textSize(128);
+					text("word", 40, 120); 
+					noFill();
+					stroke(100, 300, 100);
+					rect(map(i, 0, squares, 0, 500), 0, w2, height);
+					rect(0, map(i, 0, squares, 0, 500), map(i, 0, squares, 0, 500), height);
+					rect(0, map(i, 0, squares, 0, 500), map(i, 0, squares, 500, 500), height);
+				}
+				break;
 
 		}
 	}
