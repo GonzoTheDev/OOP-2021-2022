a82b9e2cf579954f2b61cc3a480ffd3b8d26324c
GonzoTheDev <c20703429@mytudublin.ie>
2022-02-21 11:49:28 +0000

    Update Arrays.java

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -4,8 +4,18 @@ import processing.core.PApplet;
 
 public class Arrays extends PApplet {
 
+    // First way to initialize an array
     float[] rainfall = { 45, 37, 55, 27, 38, 50, 79, 48, 104, 31, 100, 58 };
-    
+
+    // Second way to initialize an array
+    float[] a1 = new float[100];
+
+    // Third way to initialize an array
+    float[] a2; // This would produce a null pointer exception as no memory has been allocated yet
+
+
+    String[] months = {"JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC"};
+
     public void settings() 
     {
         size(500,500);
@@ -13,7 +23,16 @@ public class Arrays extends PApplet {
 
     public void setup()
     {
-        
+        for(int i = 0; i < rainfall.length; i++)
+        {
+            println(rainfall[i] + "\t" + months[i]);
+        }
+
+        // Enhanced for loop equivelant to foreach
+        for(float r:rainfall)
+        {
+            println(r);
+        }
     }
 
     public void draw()
