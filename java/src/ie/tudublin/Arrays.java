869866670fca9e65963ebee252b89ef7e9b80f23
GonzoTheDev <c20703429@mytudublin.ie>
2022-02-21 11:29:35 +0000

    create arrays project

---
+++ java/src/ie/tudublin/Arrays.java
@@ -0,0 +1,20 @@
+package ie.tudublin;
+
+import processing.core.PApplet;
+
+public class Arrays extends PApplet {
+    public void settings() 
+    {
+        size(500,00);
+    }
+
+    public void setup()
+    {
+        
+    }
+
+    public void draw()
+    {
+
+    }
+}
