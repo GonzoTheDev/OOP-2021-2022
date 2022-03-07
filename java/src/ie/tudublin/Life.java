27add53d9db2602974ec6dda5c3a7d9b2d186404
GonzoTheDev <c20703429@mytudublin.ie>
2022-03-07 11:47:34 +0000

    life

--- java/src/ie/tudublin/Life.java
+++ java/src/ie/tudublin/Life.java
@@ -4,15 +4,18 @@ import processing.core.PApplet;
 
 public class Life extends PApplet {
 
+	LifeBoard board;
+
 	public void settings() {
 		size(500, 500);
+		board = new LifeBoard(100, this);
 	}
 
 	public void setup() {
 		colorMode(HSB);
 
 	}
-
+ 
 	
 	public void draw() {
 	}
